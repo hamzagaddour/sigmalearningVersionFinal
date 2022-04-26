47f0c7bc69df912f79d7946f1a085b733ee4d6e2
GADDOUR <hamza_gaddour@yahoo.com>
2022-04-26 14:04:30 +0100

    update

--- client/src/components/teacher/TeacherDefaultPage.js
+++ client/src/components/teacher/TeacherDefaultPage.js
@@ -3,7 +3,10 @@ import img from './logo.png'
 
 const TeacherDefaultPage = () => {
   return (
+      <>
+      <h1>Welcome To Sigma Learning</h1>
     <img src={img} alt='logo'/>
+    </>
   )
 }
 
